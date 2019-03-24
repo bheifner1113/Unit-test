19d62b4eda7ac5e032152aea59291b48b0fe932d
Brian Heifner <bheifner1113@gmail.com>
2019-03-24 12:21:24 -0500

    C# Unit Test

---
+++ UnitTestProject3/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+
+namespace UnitTestProject3
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        
+
+
+
+
+        [Test]
+        public void Test1()
+        {
+            Console.Write("lol");
+        }
+    }
+}
